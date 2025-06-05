9f42c37cce0a172f57d46ab4a9e2ecb781fdfd9d
Elias <elias.manriquez.2005@gmail.com>
2025-06-04 22:31:56 -0400

    uwu

--- game/combat.h
+++ game/combat.h
@@ -1,4 +1,15 @@
 #ifndef COMBAT_H
 #define COMBAT_H
 
+typedef struct
+{
+    int ID;
+    char* nombre;
+    int hp;
+    int damage;
+    int defense;
+    int dificultad;
+}Enemigo;
+
+
 #endif
--- game/player.h
+++ game/player.h
@@ -1,4 +1,16 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include "tdas/list.h"
+
+typedef struct{
+    char* nombre;
+    List* inventario;
+    int hp;
+    int damage;
+    int defense;
+    int oro;
+    int clase;
+} Jugador;
+
 #endif
